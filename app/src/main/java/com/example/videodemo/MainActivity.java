1ef5b1f682aee211574ad0f4ddddad469ccf5cda
Dave Song <49602639+woony91@users.noreply.github.com>
2020-02-27 15:46:47 -0500

    finalized the video app

--- app/src/main/java/com/example/videodemo/MainActivity.java
+++ app/src/main/java/com/example/videodemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.videodemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
+
+        MediaController mediaController = new MediaController(this);
+
+        mediaController.setAnchorView(videoView);
+
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
+
     }
 }
